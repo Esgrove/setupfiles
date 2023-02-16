875c3360d235cc38b96dab3aedc14d66850fd77d
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-02-16 13:54:34 +0200

    improve shell profile

--- zshrc.sh
+++ zshrc.sh
@@ -157,30 +157,30 @@ alias pyupg="python3 -m pip list --not-required --format=freeze | grep -v '^\-e'
 
 # Print a message with red color
 print_red() {
-    printf "\e[1;49;31m${1}\e[0m\n"
+    printf "\e[1;49;31m%s\e[0m\n" "$1"
 }
 
 # Print a message with green color
 print_green() {
-    printf "\e[1;49;32m${1}\e[0m\n"
+    printf "\e[1;49;32m%s\e[0m\n" "$1"
 }
 
 # Print a message with yellow color
 print_yellow() {
-    printf "\e[1;49;33m${1}\e[0m\n"
+    printf "\e[1;49;33m%s\e[0m\n" "$1"
 }
 
 # Print a message with magenta color
 print_magenta() {
-    printf "\e[1;49;35m${1}\e[0m\n"
+    printf "\e[1;49;35m%s\e[0m\n" "$1"
 }
 
 print_error() {
-    print_red "ERROR: ${1}"
+    print_red "ERROR: $1"
 }
 
 print_warn() {
-    print_yellow "WARNING: ${1}"
+    print_yellow "WARNING: $1"
 }
 
 # activate Python virtual env
@@ -241,7 +241,7 @@ EOT
 
 # remove Dropbox conflicted copies
 conflicted() {
-    find ~/Dropbox/ -path "*(*'s conflicted copy [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]*" -exec rm -f {} \;
+    find "$HOME/Dropbox/" -path "*(*'s conflicted copy [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]*" -exec rm -f {} \;
 }
 
 # Eject all disk images
